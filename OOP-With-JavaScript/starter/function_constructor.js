76ec6f8a41f8bef0e4f7c62780dc547d3acc1d78
rabiulkhan7224 <mdrabiulkhanbabo@gmail.com>
2025-12-11 15:41:32 +0600

    function construcror pp done

--- OOP-With-JavaScript/starter/function_constructor.js
+++ OOP-With-JavaScript/starter/function_constructor.js
@@ -11,3 +11,13 @@ const hp= new Computer('hp',"hp23hh")
 console.log(dell)
 console.log(hp);
 dell.aboutComputer()
+
+// const obj={}
+// obj. name = "code ABC";
+// obj. playlist = 20;
+// console. Log (obj)
+// 4 rules of Function Constructor
+// 1. create an empty object { }
+// 2. function is called this = {}
+// 3. { } Linked to the prototype
+//  4.{ } will return automatically
